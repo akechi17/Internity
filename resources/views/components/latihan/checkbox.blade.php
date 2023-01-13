cdd729eec004b92387cf8b2e0c979ae0b108a014
Fanreza <rezaramdhani461@gmail.com>
2023-01-13 10:02:06 +0700

    feat: add fonts and stacked scripts

--- resources/views/components/latihan/checkbox.blade.php
+++ resources/views/components/latihan/checkbox.blade.php
@@ -3,7 +3,16 @@
     <div class="form-check">
         <input class="form-check-input" type="checkbox" value="" id="{{ $id }}">
         <label class="form-check-label" for="{{ $for }}">
-        Checkbox {{ $label }}
+            Checkbox {{ $label }}
         </label>
     </div>
 </div>
+
+{{-- Example Push Scripts to head --}}
+@once
+    @push('scripts')
+        <script type="module">
+            // Scripts here
+        </script>
+    @endpush
+@endonce

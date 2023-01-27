e2ccdc68e40dc34e0bfbc6eb9876230af7ecd877
Fanreza <rezaramdhani461@gmail.com>
2023-01-27 13:37:18 +0700

    feat: fix css not loaded in verify and reset password

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -1,13 +1,16 @@
-<div id="verify-page">
-    <div class="card d-flex align-items-center">
-        <div class="card-body">
-            <form action="{{ route('verification.resend') }}" method="POST">
-                @csrf
-                <img src="{{ asset('/img/logo-internity.png') }}" alt="Internity" />
-                <p>Before proceeding, please check your email for a verification link.</p>
-                <button type="submit" class="btn btn-link p-0 m-0 align-baseline">Click here to request another</button>.
-                <a href="#"> Back to Login </a>
-            </form>
+@extends('layouts.app')
+
+@section('content')
+    <div id="verify-page">
+        <div class="card d-flex align-items-center">
+            <div class="card-body">
+                <form action="{{ route('verification.resend') }}" method="POST">
+                    @csrf
+                    <img src="{{ asset('/img/logo-internity.png') }}" alt="Internity" />
+                    <p>Before proceeding, please check your email for a verification link.</p>
+                    <button type="submit" class="btn btn-link p-0 m-0 align-baseline">Click here to request another</button>.
+                </form>
+            </div>
         </div>
     </div>
-</div>
+@endsection

53d0490fbe3a1d34b4dde35a476a84a2bc7d162b
tsanyaaz <tsanyaaz@gmail.com>
2023-01-27 10:50:05 +0700

    verify and reset password scss

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -3,7 +3,7 @@
         <div class="card-body">
             <form action="{{ route('verification.resend') }}" method="POST">
                 @csrf
-                <img src="img/logo-internity.png" alt="Internity" />
+                <img src="{{ asset('/img/logo-internity.png') }}" alt="Internity" />
                 <p>Before proceeding, please check your email for a verification link.</p>
                 <button type="submit" class="btn btn-link p-0 m-0 align-baseline">Click here to request another</button>.
                 <a href="#"> Back to Login </a>

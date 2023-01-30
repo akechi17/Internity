e72031d870b5208f29a14a048e890168da79a036
tsanyaaz <tsanyaaz@gmail.com>
2023-01-30 09:59:56 +0700

    edit style template

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -6,7 +6,7 @@
         <i class="fas fa-times p-3 cursor-pointer text-secondary opacity-5 position-absolute end-0 top-0 d-none d-xl-none"
             aria-hidden="true" id="iconSidenav"></i>
         <a class="navbar-brand m-0" href="dashboard.html ">
-            <img src="../assets/img/logo-internity.png" class="navbar-brand-img h-100" alt="main_logo" />
+            <img src="{{ asset('/img/logo-internity.png') }}" class="navbar-brand-img h-100" alt="main_logo" />
             <span class="ms-1 font-weight-bold">INTERNITY</span>
         </a>
     </div>
@@ -23,13 +23,7 @@
                     <a class="nav-link active" href="">
                         <div
                             class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                                class="bi bi-speedometer" viewBox="0 0 16 16">
-                                <path
-                                    d="M8 2a.5.5 0 0 1 .5.5V4a.5.5 0 0 1-1 0V2.5A.5.5 0 0 1 8 2zM3.732 3.732a.5.5 0 0 1 .707 0l.915.914a.5.5 0 1 1-.708.708l-.914-.915a.5.5 0 0 1 0-.707zM2 8a.5.5 0 0 1 .5-.5h1.586a.5.5 0 0 1 0 1H2.5A.5.5 0 0 1 2 8zm9.5 0a.5.5 0 0 1 .5-.5h1.5a.5.5 0 0 1 0 1H12a.5.5 0 0 1-.5-.5zm.754-4.246a.389.389 0 0 0-.527-.02L7.547 7.31A.91.91 0 1 0 8.85 8.569l3.434-4.297a.389.389 0 0 0-.029-.518z" />
-                                <path fill-rule="evenodd"
-                                    d="M6.664 15.889A8 8 0 1 1 9.336.11a8 8 0 0 1-2.672 15.78zm-4.665-4.283A11.945 11.945 0 0 1 8 10c2.186 0 4.236.585 6.001 1.606a7 7 0 1 0-12.002 0z" />
-                            </svg>
+                            <i class="bi bi-speedometer"></i>
                         </div>
                         <span class="nav-link-text ms-1">{{ $item->name }}</span>
                     </a>

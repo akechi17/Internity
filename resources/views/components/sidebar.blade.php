0caa4c309cb4ef254ad5ead97418c7e9dcb71368
Hermawan S Sarjito <hermawansent@gmail.com>
2023-01-30 10:50:18 +0700

    feat: route in sidebar

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -20,7 +20,7 @@
             <!-- Nav-Dashboard Start-->
             @foreach ($menus as $item)
                 <li class="nav-item">
-                    <a class="nav-link active" href="">
+                    <a class="nav-link active" href="/{{ $item->url }}">
                         <div
                             class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
                             <i class="bi bi-speedometer"></i>

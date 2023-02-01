07bf75ad33a74bc2e8cfa09d4f7c1c871df0a1b8
Fanreza <rezaramdhani461@gmail.com>
2023-02-01 10:04:19 +0700

    feat: add custom pagination style

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -7,6 +7,10 @@
 
 
     {{-- dashboard content --}}
-    @yield('dashboard-content')
+    <main class="main-content position-relative max-height-vh-100 h-100 border-radius-lg">
+        <div class="container-fluid py-4">
+            @yield('dashboard-content')
+        </div>
+    </main>
     {{-- Dashboard content --}}
 </div>
--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -5,46 +5,29 @@
 @extends('layouts.dashboard')
 
 @section('dashboard-content')
-    {{-- search bar --}}
-    <form action="{{ route('users.index') }}" method="GET">
-        <div class="input-group mb-3">
-            <input type="text" class="form-control" placeholder="Search" name="search" value="{{ request()->search }}">
-            <div class="input-group-append">
-                <button class="btn btn-primary" type="submit">Search</button>
-            </div>
-        </div>
-    </form>
+    <x-table targetTambah="kldgnlskngs" idTambah="1" labelTambah="asgasgsa" targetEdit="dgsdg" idEdit="2"
+        labelEdit="asfsafas" pageName="asgasgas" :pagination="$users">
 
-    {{-- button add --}}
-    <a href="{{ route('users.create') }}" class="btn btn-primary">Tambah</a>
-
-    <table class="table table-dark table-striped">
-        <thead>
+        <x-slot:thead>
             <th>Aksi</th>
             <th>Nama</th>
             <th>Email</th>
             <th>Last Login</th>
             <th>Last Login IP</th>
-        </thead>
+        </x-slot:thead>
 
-        <tbody>
-            {{-- data yang dikasih di loop dulu --}}
+        <x-slot:tbody>
             @foreach ($users as $user)
                 <tr>
-                    {{-- button edit --}}
                     <td>
                         <a href="{{ route('users.edit', encrypt($user->id)) }}" class="btn btn-primary">Edit</a>
                     </td>
-                    {{-- disini dia bisa ditampilin sesuai property nya --}}
                     <td>{{ $user->name }}</td>
                     <td>{{ $user->email }}</td>
                     <td>{{ $user->last_login }}</td>
                     <td>{{ $user->last_login_ip }}</td>
                 </tr>
             @endforeach
-        </tbody>
-    </table>
-
-    {{-- ini untuk pagination --}}
-    {{ $users->links() }}
+        </x-slot:tbody>
+    </x-table>
 @endsection

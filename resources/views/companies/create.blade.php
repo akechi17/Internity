aafe7f99c3789a44f4eb0618518f1a688f37ba8a
tsanyaaz <tsanyaaz@gmail.com>
2023-02-06 12:40:58 +0700

    edit index companies

---
+++ resources/views/companies/create.blade.php
@@ -0,0 +1,65 @@
+{{-- @extends('layouts.dashboard')
+
+@section('dashboard-content')
+<div class="row">
+        <div class="card mb-4">
+            <div class="card-header pb-0">
+                <h6 class="pb-3">PERUSAHAAN</h6>
+                <form action="#" method="#">
+                    <div class="row">
+                        <div class="col-md-4 pb-3 form-group">
+                            <label for="input-data" class="form-label">Logo</label>
+                            <input type="file" class="form-control" id="input-data"/>
+                        </div>
+                        <div class="col-md-4 form-group">
+                            <label for="input-data">Nama</label>
+                            <input type="text" class="form-control" id="input-data" required/>
+                        </div>
+                        <div class="col-md-4 form-group">
+                            <label for="input-data">Kategori</label>
+                            <input type="text" class="form-control" id="input-data" required/>
+                        </div>
+                        <div class="col-md-6 pb-3 form-group">
+                            <label for="input-data">Contact Person</label>
+                            <input type="text" class="form-control" id="input-data" required/>
+                        </div>
+                        <div class="col-md-6 form-group">
+                            <label for="input-data">Phone</label>
+                            <input type="text" class="form-control" id="input-data" required/>
+                        </div>
+                        <div class="col-md-6 pb-3 form-group">
+                            <label for="input-data">Email</label>
+                            <input type="email" class="form-control" id="input-data" required/>
+                        </div>
+                        <div class="col-md-6 form-group">
+                            <label for="input-data">Website</label>
+                            <input type="text" class="form-control" id="input-data" required/>
+                        </div>
+                        <div class="col-md-6 pb-3 form-group">
+                            <label for="input-data">Alamat</label>
+                            <input type="text" class="form-control" id="input-data"/>
+                        </div>
+                        <div class="col-md-6 form-group">
+                            <label for="input-data">Kota</label>
+                            <input type="text" class="form-control" id="input-data"/>
+                        </div>
+                        <div class="col-md-6 pb-3 form-group">
+                            <label for="input-data">Provinsi</label>
+                            <input type="text" class="form-control" id="input-data"/>
+                        </div>
+                        <div class="col-md-6 form-group">
+                            <label for="input-data">Negara</label>
+                            <input type="text" class="form-control" id="input-data"/>
+                        </div>
+                        <button type="button" class="btn bg-gradient-info w-10" id="save-data" onclick="#">
+                            Simpan
+                        </button>
+                        <button type="button" class="btn bg-gradient-danger w-10" id="cancel" onclick="#">
+                            Batal
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
+</div>
+@endsection --}}

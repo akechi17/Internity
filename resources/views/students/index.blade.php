8bf64aebf089ef2b2ef206c6295b8043b4edbf33
rimaoktav <rima.oktaviani7e28@gmail.com>
2023-02-21 12:47:12 +0700

    add index.student

--- resources/views/students/index.blade.php
+++ resources/views/students/index.blade.php
@@ -8,25 +8,25 @@
     <x-table pageName="Data Magang Siswa" route="{{ route('users.create') }}" :pagination="$students" :tableData="$students">
 
         <x-slot:thead>
-            <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 w-20">
+            <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 w-20">
                 Kelola
             </th>
-            <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 w-10">
+            <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 w-15">
                 Nama
             </th>
-            <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 w-15">
+            <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 w-10">
                 Kelas
             </th>
-            <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 w-10">
+            <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 w-10">
                 DU/DI
             </th>
-            <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 w-10">
+            <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 w-10">
                 Tanggal Mulai
             </th>
-            <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 w-10">
+            <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 w-10">
                 Tanggal Selesai
             </th>
-            <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 w-10">
+            <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 w-10">
                 Status Perpanjang
             </th>
         </x-slot:thead>
@@ -56,11 +56,11 @@
                                 <a href="{{ route('users.edit', encrypt($student->id)) }}" class="btn btn-primary text-xs">Jurnal</a>
                             </td>
                             <td class="text-sm">{{ $student->name }}</td>
-                            <td class="text-sm">{{ $student->courses->first()?->name }}</td>
+                            <td class="text-sm text-center">{{ $student->courses->first()?->name }}</td>
                             <td class="text-sm">{{ $company->name }}</td>
-                            <td class="text-sm">{{ $student->internDates()->where('company_id', $company->id)->first()?->start_date }}</td>
-                            <td class="text-sm">{{ $student->internDates()->where('company_id', $company->id)->first()?->end_date }}</td>
-                            <td class="text-sm">{{ $student->internDates()->where('company_id', $company->id)->first()?->extend }}</td>
+                            <td class="text-sm text-center">{{ $student->internDates()->where('company_id', $company->id)->first()?->start_date }}</td>
+                            <td class="text-sm text-center">{{ $student->internDates()->where('company_id', $company->id)->first()?->end_date }}</td>
+                            <td class="text-sm text-center">{{ $student->internDates()->where('company_id', $company->id)->first()?->extend }}</td>
                         </tr>
                     @endforeach
                 @endif

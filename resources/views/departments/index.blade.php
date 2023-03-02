f052958721e3cef1a8c6898d0693fb4a885835ab
Hermawan S Sarjito <hermawansent@gmail.com>
2023-03-02 23:07:57 +0700

    refactor deparmtent

--- resources/views/departments/index.blade.php
+++ resources/views/departments/index.blade.php
@@ -9,7 +9,7 @@
         permissionCreate="department-create" :pagination="$departments" :tableData="$departments">
 
         <x-slot:thead>
-            <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 w-15">
+            <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 w-10">
                 Kelola
             </th>
             <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 w-35">
@@ -23,9 +23,14 @@
         <x-slot:tbody>
             @foreach ($departments as $data)
                 <tr>
-                    <td class="d-flex justify-content-center">
+                    <td class="text-center">
+                        @can('course-list')
+                            <a href="{{ route('courses.index', ['department' => encrypt($data->id)]) }}" class="btn btn-info text-xs"
+                                data-bs-toggle="tooltip" data-bs-placement="bottom" title="Kelas"><i
+                                    class="bi bi-book-half"></i></a>
+                        @endcan
                         @can('department-edit')
-                            <a href="{{ route('departments.edit', encrypt($data->id)) }}" class="btn btn-info text-xs me-2"
+                            <a href="{{ route('departments.edit', encrypt($data->id)) }}" class="btn btn-info text-xs"
                                 data-bs-toggle="tooltip" data-bs-placement="bottom" title="Edit"><i
                                     class="bi bi-pencil-square"></i></a>
                         @endcan
@@ -33,15 +38,10 @@
                             <form action="{{ route('departments.destroy', encrypt($data->id)) }}" method="POST" class="m-0">
                                 @csrf
                                 @method('DELETE')
-                                <button id="button-{{ $data->id }}" class="button-delete btn btn-info text-xs"
+                                <button id="button-{{ $data->id }}" class="button-delete btn btn-danger text-xs"
                                     data-bs-toggle="tooltip" data-bs-placement="bottom" title="Delete" type="button"><i
                                         class="bi bi-trash"></i></button>
                             </form>
-                        @endcan
-                        @can('course-list')
-                            <a href="{{ route('courses.index', ['department' => encrypt($data->id)]) }}" class="btn btn-info text-xs ms-2"
-                                data-bs-toggle="tooltip" data-bs-placement="bottom" title="Kelas"><i
-                                    class="bi bi-book-half"></i></a>
                         @endcan
                     </td>
                     <td class="text-sm text-center">{{ $data->name }}</td>

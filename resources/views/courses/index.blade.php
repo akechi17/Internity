a717f9a38d73a3da295b343b224b1c78070ac737
tsanyaaz <tsanyaaz@gmail.com>
2023-03-02 23:08:12 +0700

    add swal approve

--- resources/views/courses/index.blade.php
+++ resources/views/courses/index.blade.php
@@ -3,13 +3,13 @@
 @section('dashboard-content')
     <x-table routeCreate="{{ route('courses.create', ['department'=> encrypt($departmentId)]) }}" pageName="Kelas" :pagination="$courses" :tableData="$courses">
         <x-slot:thead>
-            <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 w-5">
+            <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 w-35">
                 Kelola
             </th>
-            <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 w-14">
+            <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 w-15">
                 Nama
             </th>
-            <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 w-5">
+            <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 w-50">
                 Deskripsi
             </th>
         </x-slot:thead>
@@ -17,20 +17,20 @@
         <x-slot:tbody>
             @foreach ($courses as $data)
                 <tr>
-                    <td class="text-center">
-                        <a href="{{ route('courses.edit', encrypt($data->id)) }}" class="btn btn-info text-xs"
+                    <td class="d-flex justify-content-center">
+                        <a href="{{ route('courses.edit', encrypt($data->id)) }}" class="btn btn-info text-xs me-1"
                             data-bs-toggle="tooltip" data-bs-placement="bottom" title="Edit"><i
                                 class="bi bi-pencil-square"></i></a>
 
                         <form action="{{ route('courses.destroy', encrypt($data->id)) }}" method="POST" class="m-0">
                             @csrf
                             @method('DELETE')
-                            <button id="button-{{ $data->id }}" class="button-delete btn btn-info text-xs"
+                            <button id="button-{{ $data->id }}" class="button-delete btn btn-info text-xs ms-1"
                                 data-bs-toggle="tooltip" data-bs-placement="bottom" title="Delete" type="button"><i
                                     class="bi bi-trash"></i></button>
                         </form>
                     </td>
-                    <td class="text-sm">{{ $data->name }}</td>
+                    <td class="text-sm text-center">{{ $data->name }}</td>
                     <td class="text-sm">{{ $data->description }}</td>
                 </tr>
             @endforeach

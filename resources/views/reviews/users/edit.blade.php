c95ce4d9313ba2da5043838a091a782f28eed13a
Fanreza <rezaramdhani461@gmail.com>
2023-03-05 13:59:49 +0700

    fix: fix bug sharing same name in radio button review

--- resources/views/reviews/users/edit.blade.php
+++ resources/views/reviews/users/edit.blade.php
@@ -1,36 +1,41 @@
 @extends('layouts.dashboard')
 
 @section('dashboard-content')
-    <x-form.form formTitle="Kuisioner" formMethod="POST" spoofMethod="PUT"
-        formAction="{{ route('reviews.users.update') }}">
+    <x-form.form formTitle="Kuisioner" formMethod="POST" spoofMethod="PUT" formAction="{{ route('reviews.users.update') }}">
         <x-slot:formBody>
             @foreach ($reviews as $review)
                 <input type="hidden" name="reviews[id][]" value="{{ $review->id }}">
                 <x-form.radio label="{{ $review->title }}" name="reviews[rating][]" value="{{ $review->rating }}">
                     <x-slot:checkboxItem>
                         <div class="form-check">
-                            <input class="form-check-input" type="radio" value="5" id="input-status-1"
-                                {{ $review->rating == 5 ? 'checked' : '' }}>
-                            <label class="form-check-label" for="input-status-1">Baik Sekali</label>
+                            <input class="form-check-input" type="radio" value="5"
+                                id="input-review-5-{{ $review->id }}" {{ $review->rating == 5 ? 'checked' : '' }}
+                                name="reviews[rating][{{ $review->id }}]">
+                            <label class="form-check-label" for="input-review-5-{{ $review->id }}">Baik Sekali</label>
                         </div>
                         <div class="form-check">
-                            <input class="form-check-input" type="radio" value="4" id="input-status-2"
-                                {{ $review->rating == 4 ? 'checked' : '' }}>
-                            <label class="form-check-label" for="input-status-2">Baik</label>
+                            <input class="form-check-input" type="radio" value="4"
+                                id="input-review-4-{{ $review->id }}" {{ $review->rating == 4 ? 'checked' : '' }}
+                                name="reviews[rating][{{ $review->id }}]">
+                            <label class="form-check-label" for="input-review-4-{{ $review->id }}">Baik</label>
                         </div>
                         <div class="form-check">
-                            <input class="form-check-input" type="radio" value="3" id="input-status-3"
-                                {{ $review->rating == 3 ? 'checked' : '' }}>
-                            <label class="form-check-label" for="input-status-2">Cukup</label>
-                        </div><div class="form-check">
-                            <input class="form-check-input" type="radio" value="2" id="input-status-4"
-                                {{ $review->rating == 2 ? 'checked' : '' }}>
-                            <label class="form-check-label" for="input-status-2">Kurang</label>
+                            <input class="form-check-input" type="radio" value="3"
+                                id="input-review-3-{{ $review->id }}" {{ $review->rating == 3 ? 'checked' : '' }}
+                                name="reviews[rating][{{ $review->id }}]">
+                            <label class="form-check-label" for="input-review-3-{{ $review->id }}">Cukup</label>
                         </div>
                         <div class="form-check">
-                            <input class="form-check-input" type="radio" value="1" id="input-status-5"
-                                {{ $review->rating == 1 ? 'checked' : '' }}>
-                            <label class="form-check-label" for="input-status-2">Kurang Sekali</label>
+                            <input class="form-check-input" type="radio" value="2"
+                                id="input-review-2-{{ $review->id }}" {{ $review->rating == 2 ? 'checked' : '' }}
+                                name="reviews[rating][{{ $review->id }}]">
+                            <label class="form-check-label" for="input-review-2-{{ $review->id }}">Kurang</label>
+                        </div>
+                        <div class="form-check">
+                            <input class="form-check-input" type="radio" value="1"
+                                id="input-review-1-{{ $review->id }}" {{ $review->rating == 1 ? 'checked' : '' }}
+                                name="reviews[rating][{{ $review->id }}]">
+                            <label class="form-check-label" for="input-review-1-{{ $review->id }}">Kurang Sekali</label>
                         </div>
                     </x-slot:checkboxItem>
                 </x-form.radio>

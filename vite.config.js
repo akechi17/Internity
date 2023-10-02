8d1bb246d7216e18beff23b36eb09d542876ed47
Fanreza <rezaramdhani461@gmail.com>
2023-10-02 14:41:29 +0700

    feat(chunks asset): add options to split module and add hash to chunk to prevent caching when website updated

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from "vite";
 import laravel from "laravel-vite-plugin";
 import path from "path";
 
+const hash = Math.floor(Math.random() * 90000) + 10000;
+
 export default defineConfig({
     server: {
         host: true,
@@ -24,6 +26,25 @@ export default defineConfig({
                 "node_modules/bootstrap-icons/font"
             ),
             "~trix": path.resolve(__dirname, "node_modules/trix"),
+        },
+    },
+
+    build: {
+        rollupOptions: {
+            output: {
+                manualChunks(id) {
+                    if (id.includes("node_modules")) {
+                        return id
+                            .toString()
+                            .split("node_modules/")[1]
+                            .split("/")[0]
+                            .toString();
+                    }
+                },
+                entryFileNames: `[name]${hash}.js`,
+                chunkFileNames: `[name]${hash}.js`,
+                assetFileNames: `[name]${hash}.[ext]`,
+            },
         },
     },
 });
